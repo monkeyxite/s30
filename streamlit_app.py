f2f9ae6696ce3512f1f57f8ecfd8e06970bda4d4
Jonny Hou <jonny.hou@ericsson.com>
2022-10-16 20:18:55 +0200

    D12 done.

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,11 +90,22 @@ import streamlit as st
 # st.write('Your favorite color is ', option)
 
 # D11
-st.header('st.multiselect')
+# st.header('st.multiselect')
+# options = st.multiselect(
+#      'What are your favorite colors',
+#      ['Green', 'Yellow', 'Red', 'Blue'],
+#      ['Yellow', 'Red'])
+# st.write('You selected:', options)
 
-options = st.multiselect(
-     'What are your favorite colors',
-     ['Green', 'Yellow', 'Red', 'Blue'],
-     ['Yellow', 'Red'])
-
-st.write('You selected:', options)
+# D12
+st.header('st.checkbox')
+st.write ('What would you like to order?')
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+if icecream:
+     st.write("Great! Here's some more 🍦")
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+if cola:
+     st.write("Here you go 🥤")
