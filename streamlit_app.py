aca2bfd70426f6b3d10e8e61bb9c0b4b9136b9df
Jonny Hou <jonny.hou@ericsson.com>
2022-10-16 09:33:20 +0200

    Done D3.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.write('Hello world!')
+# D2
+# st.write('Hello world!')
+
+# D3
+st.header('st.button')
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
