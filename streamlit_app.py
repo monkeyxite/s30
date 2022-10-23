d966a650a2fcdc272f0f7c23c58e03b79f6c60a9
Jonny Hou <jonny.hou@ericsson.com>
2022-10-23 09:52:54 +0200

    D22 done

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,17 +205,59 @@ import streamlit as st
 #     st.write('👈 Please choose your favorite **food**!')
 
 # D21
-import time
+# import time
 
-st.title('st.progress')
+# st.title('st.progress')
 
-with st.expander('About this app'):
-     st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
+# with st.expander('About this app'):
+#      st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
 
-my_bar = st.progress(0)
+# my_bar = st.progress(0)
 
-for percent_complete in range(100):
-     time.sleep(0.05)
-     my_bar.progress(percent_complete + 1)
+# for percent_complete in range(100):
+#      time.sleep(0.05)
+#      my_bar.progress(percent_complete + 1)
 
-st.balloons()
+# st.balloons()
+
+# D22 
+st.title('st.form')
+
+# Full example of using the with notation
+st.header('1. Example of using `with` notation')
+st.subheader('Coffee machine')
+
+with st.form('my_form'):
+    st.subheader('**Order your coffee**')
+    # Input widgets
+    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
+    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
+    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
+    serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
+    milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
+    owncup_val = st.checkbox('Bring own cup')
+    # Every form must have a submit button
+    submitted = st.form_submit_button('Submit')
+
+if submitted:
+    st.markdown(f'''
+        ☕ You have ordered:
+        - Coffee bean: `{coffee_bean_val}`
+        - Coffee roast: `{coffee_roast_val}`
+        - Brewing: `{brewing_val}`
+        - Serving type: `{serving_type_val}`
+        - Milk: `{milk_val}`
+        - Bring own cup: `{owncup_val}`
+        ''')
+else:
+    st.write('☝️ Place your order!')
+
+
+# Short example of using an object notation
+st.header('2. Example of object notation')
+
+form = st.form('my_form_2')
+selected_val = form.slider('Select a value')
+form.form_submit_button('Submit')
+
+st.write('Selected value: ', selected_val)
