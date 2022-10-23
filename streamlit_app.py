762a57c8c349e8980cf18cb3e5a31082a2e26f52
Jonny Hou <jonny.hou@ericsson.com>
2022-10-23 09:38:27 +0200

    D18 done.

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,7 +149,22 @@ import streamlit as st
 # st.write('Selected number from slider widget is:', number)
 
 # D17
-st.title('st.secrets')
-st.write(st.secrets['message'])
+# st.title('st.secrets')
+# st.write(st.secrets['message'])
 
+# D19
+import pandas as pd
 
+st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
