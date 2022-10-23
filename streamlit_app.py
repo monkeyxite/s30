46fd0dec5b36944b72f4f5740900de3997dbfec1
Jonny Hou <jonny.hou@ericsson.com>
2022-10-23 09:42:58 +0200

    D19 done.

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,18 +153,53 @@ import streamlit as st
 # st.write(st.secrets['message'])
 
 # D19
-import pandas as pd
+# import pandas as pd
 
-st.title('st.file_uploader')
+# st.title('st.file_uploader')
 
-st.subheader('Input CSV')
-uploaded_file = st.file_uploader("Choose a file")
+# st.subheader('Input CSV')
+# uploaded_file = st.file_uploader("Choose a file")
 
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.subheader('DataFrame')
-  st.write(df)
-  st.subheader('Descriptive Statistics')
-  st.write(df.describe())
-else:
-  st.info('☝️ Upload a CSV file')
+# if uploaded_file is not None:
+#   df = pd.read_csv(uploaded_file)
+#   st.subheader('DataFrame')
+#   st.write(df)
+#   st.subheader('Descriptive Statistics')
+#   st.write(df.describe())
+# else:
+#   st.info('☝️ Upload a CSV file')
+
+# D19
+st.set_page_config(layout="wide")
+st.title('How to layout your Streamlit app')
+
+with st.expander('About this app'):
+  st.write('This app shows the various ways on how you can layout your Streamlit app.')
+  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+
+st.sidebar.header('Input')
+user_name = st.sidebar.text_input('What is your name?')
+user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+
+st.header('Output')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  if user_name != '':
+    st.write(f'👋 Hello {user_name}!')
+  else:
+    st.write('👈  Please enter your **name**!')
+
+with col2:
+  if user_emoji != '':
+    st.write(f'{user_emoji} is your favorite **emoji**!')
+  else:
+    st.write('👈 Please choose an **emoji**!')
+
+with col3:
+  if user_food != '':
+    st.write(f'🍴 **{user_food}** is your favorite **food**!')
+  else:
+    st.write('👈 Please choose your favorite **food**!')
